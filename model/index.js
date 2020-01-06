2e7752f1ea18fd4107f8e98708caf62425638d12
heeking13 <513301743@qq.com>
2020-01-06 18:21:41 +0000

    version 2

---
+++ model/index.js
@@ -0,0 +1,24 @@
+//Date: 20/12/2019
+//This file is about the operation of mongoDB
+
+// cited from https://www.npmjs.com/package/mongodb
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017";
+var database = "project";//connect the database "report"
+
+// Use connect method to connect to the server
+function connect(callback) {
+  MongoClient.connect(url, function(err, client) {
+    if (err) {
+      console.log("Databse connected failed", err);
+    } else {
+      var db = client.db(database);
+      callback && callback(db);
+      client.close();
+    }
+  });
+}
+
+module.exports = {
+  connect
+};
